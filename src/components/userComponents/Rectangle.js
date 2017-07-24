19b573075df651f3b75973844d0b865bf451fa7f
Syed Tauseef Tanweer <Syed Tauseef Tanweer>
2017-07-24 23:12:38 +0530

    build script added

--- src/components/userComponents/Rectangle.js
+++ src/components/userComponents/Rectangle.js
@@ -11,8 +11,7 @@ export default class Rectangle extends Component {
                 <svg width="50" height="50">
                     <rect width="50" height="50" className="rectClass"/>     
                      <polygon points="0,50 25,25 50,50" className="triangleClass"/>           
-                </svg>
-                
+                </svg>                
             </div>
         );
     }
